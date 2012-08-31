1372ea0ec123df8cdb95a2100f363f6402446427
Aaron Miller <apage43@ninjawhale.com>
2012-08-31 12:40:10 -0700

    rate limiting happens on caller thread

--- src/grab_bag/time.clj
+++ src/grab_bag/time.clj
@@ -1,25 +1,20 @@
 (ns grab-bag.time)
 
-(defn now [] (System/currentTimeMillis))
-
-(defn rate-limited-promises
-  "Wrap the passed function `f` and allow it to be called no more than once
-  every `minwait` milliseconds. Instead of the result, return a promise."
-  [minwait f]
-  (let [timeagent (agent 0)]
-    (fn [& args]
-      (let [result (promise)]
-        (send-off timeagent (fn [last-time]
-                              (let [current (now)
-                                    at-least (+ last-time minwait)]
-                                (when (< current at-least)
-                                  (Thread/sleep (- at-least current))))
-                              (deliver result (apply f args))
-                              (now)))
-        result))))
+(defn now-ms
+  "Now, in milliseconds."
+  [] (System/currentTimeMillis))
 
 (defn rate-limited
-  "Like (rate-limited-promises) but will deref the promise and always return the
-   result, blocking until it is available."
+  "Wrap `f' to create a fn that will call f at most once every minwait
+  milliseconds."
   [minwait f]
-  (comp deref (rate-limited-promises minwait f)))
+  (let [timeatom (atom 0)]
+    (fn [& args]
+      (locking timeatom
+                (let [at-least (+ @timeatom minwait)
+                      now (now-ms)]
+                  (when (< now at-least)
+                    (Thread/sleep (- at-least now)))
+                  (reset! timeatom (now-ms))))
+      (apply f args))))
+
